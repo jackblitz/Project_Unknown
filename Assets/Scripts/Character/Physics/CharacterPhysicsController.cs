5558c951f7d57ebe68cfde8cbd6bcd6f0b758f09
Luke <lukehopkinsdev@gmail.com>
2021-01-24 22:18:10 -0800

    Active Ragdoll

--- Assets/Scripts/Character/Physics/CharacterPhysicsController.cs
+++ Assets/Scripts/Character/Physics/CharacterPhysicsController.cs
@@ -6,30 +6,35 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterMotor))]
 public class CharacterPhysicsController : MonoBehaviour
 {
-    private Rigidbody mBody;
+    public Rigidbody mBody;
     private CharacterMotor mMoveMotor;
+
+    public Animator mAnimator;
+    public CharacterController mCharacterController;
+
     // Start is called before the first frame update
     void Start()
     {
-        mBody = GetComponent<Rigidbody>();
+        //mBody = GetComponent<Rigidbody>();
         mMoveMotor = GetComponent<CharacterMotor>();
+      // mCharacterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        if (mBody.velocity.magnitude > 4f)
+        if (mBody.velocity.magnitude > 3f)
         {
-            mBody.velocity = Vector3.ClampMagnitude(mBody.velocity, 4f);
+           // mBody.velocity = Vector3.ClampMagnitude(mBody.velocity, 3.2f);
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        mBody.AddForce(mMoveMotor.Direction, ForceMode.Impulse);
-
+        mBody.AddForce(mMoveMotor.Direction * 2.2f, ForceMode.Impulse);
+       // mCharacterController.Move(mMoveMotor.Direction);
         //transform.forward = mBody.velocity.normalized;
-
+        mAnimator.SetFloat("Speed", mMoveMotor.Direction.magnitude);
         Debug.Log(mMoveMotor.Position);
     }
 }
